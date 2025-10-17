2480822d39e1ac97048c773c620e3aadf4a6406a
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-10-17 09:34:06 -0300

    chore: add initial backend folder setup with a simple express server

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import { config } from "dotenv";
+import express from "express";
+
+config();
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
